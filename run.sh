dcee35aaac4df46d6f7aae029c5930c0564139df
Blue-Davinci <brayancarter67@gmail.com>
2025-07-10 17:27:10 +0300

    Feat: Headless update - Added --nohup flag, now users can use it to follow a complete headless experience. - cli now supports nohup, hidden and normal run states.

--- run.sh
+++ run.sh
@@ -4,4 +4,12 @@ if ! command -v python3 &>/dev/null; then echo "python3 not found"; exit 1; fi
 [ ! -d "venv" ] && python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python cli.py "$@" 
+
+if [ "$1" == "--nohup" ]; then
+    nohup python cli.py --hidden > /dev/null 2>&1 &
+    echo "Process started with PID: $!"
+elif [ "$1" == "--hidden" ]; then
+    python cli.py --hidden
+else
+    python cli.py
+fi
